57e7f73a8b0be3cb3c959d50fa5646a9b0a7e04f
Łukasz Marcińczak <cqyp223@gmail.com>
2018-04-22 20:56:33 +0200

    pierwsza wersja

--- udp-broadcast.py
+++ udp-broadcast.py
@@ -6,17 +6,19 @@
 
 import sys
 import socket
+import os
+import subprocess
 
 
 def help():
 	print("\nUżycie:")
 	print("{} -ip=<adres ip> -p=<port>\n".format(sys.argv[0]))
-	print("\t--address_ip lub -ip - adres ip")
-	print("\t--port\t     lub -p  - port\n")
+	print("\t--address_ip lub -ip - adres ip (domyślnie: 255.255.255.255)")
+	print("\t--port\t     lub -p  - port\t(domyślnie: 1234)\n")
 
 def main(argv):
-	ip = ""
-	port = 0
+	ip = "255.255.255.255"
+	port = 1234
 	if len(argv) == 1:
 		help()
 		sys.exit()
@@ -36,7 +38,7 @@ def main(argv):
 		elif (argumenty[i][:6] == "--port") or (argumenty[i][:2] == "-p"):
 			temp = argumenty[i].split("=")
 			try:
-				port = temp[1]
+				port = int(temp[1])
 			except IndexError:
 				print("Niepoprawny argument: --port (-p)")
 				sys.exit()
@@ -44,7 +46,15 @@ def main(argv):
 			print("Niepoprawny argument")
 			sys.exit()
 	
-	print(ip, port)
+	print("Streaming at IP {}, PORT {}".format(ip, port))
+	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+	pipe_r, pipe_w = os.pipe()
+	process = subprocess.Popen(["ffmpeg", "-re", "-video_size", "1920x1080", "-framerate", "30", "-f", "x11grab", "-i", ":0.0", "-c:v", "mpeg2video", "-crf", "0", "-preset", "ultrafast", "-maxrate", "20M", "-b:v", "10M", "-f", "mpegts", "-"], stdout=pipe_w)
+	#print(process.comunicate()[0])
+	data = os.fdopen(pipe_r)
+	message = data.read()
+	while 1: # główna pętla programu
+		sock.sendto(message, (ip, port)) # wysyłanie datagramu UDP
 
 
 if __name__ == "__main__":
