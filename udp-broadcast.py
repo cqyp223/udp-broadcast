a7245c1c1c89cd703d5b95f01576d29b194bbd26
Łukasz Marcińczak <cqyp223@gmail.com>
2018-04-25 11:50:20 +0200

    dodano fork()

--- udp-broadcast.py
+++ udp-broadcast.py
@@ -47,15 +47,24 @@ def main(argv):
 			sys.exit()
 	
 	print("Streaming at IP {}, PORT {}".format(ip, port))
-	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
-	pipe_r, pipe_w = os.pipe()
-	process = subprocess.Popen(["ffmpeg", "-re", "-video_size", "1920x1080", "-framerate", "30", "-f", "x11grab", "-i", ":0.0", "-c:v", "mpeg2video", "-crf", "0", "-preset", "ultrafast", "-maxrate", "20M", "-b:v", "10M", "-f", "mpegts", "-"], stdout=pipe_w)
-	#print(process.comunicate()[0])
-	data = os.fdopen(pipe_r)
-	while 1: # główna pętla programu
-		message = data.read(1024)
-		if message:
-			sock.sendto(message, (ip, port)) # wysyłanie datagramu UDP
+	
+	pipe = ""
+	if not os.path.exists(pipe):
+		os.mkfifo(pipe)
+		
+	pid = os.fork()
+	if pid != 0:
+		# parent()
+		pipe_in = open(pipe, "rb")
+		sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+		while 1:
+			data = pipe_in.read(1024)
+			if data:
+				sock.sendto(data, (ip, port)) # Wysyłanie datagramu UDP
+	else:
+		# child()
+		pipe_out = os.open(pipe, os.O_WRONLY)
+		process = subprocess.Popen(["ffmpeg", "-re", "-video_size", "1920x1080", "-framerate", "30", "-f", "x11grab", "-i", ":0.0", "-c:v", "mpeg2video", "-crf", "0", "-preset", "ultrafast", "-maxrate", "20M", "-b:v", "10M", "-f", "mpegts", pipe_out])
 
 
 if __name__ == "__main__":
